6009ddceb9bf8b9c440fa239f7a9e93fb28ce88b
AdusumilliNeelima <157205816+AdusumilliNeelima@users.noreply.github.com>
2024-07-02 09:33:49 +0530

    Day-10.py

--- Day-10.py
+++ Day-10.py
@@ -1,3 +1,4 @@
+
 # leetcode-94,104,102,144,145,41,53
 #53-maximum sumarray
 class Solution:
@@ -11,5 +12,53 @@ class Solution:
             if curr_sum<0:
                 curr_sum=0
         return max_sum
-
+#94-binary tree inorder traversal
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res=[]
+        def inorder(root):
+            if root==None:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return(res)
+#145-binary tree postorder traversal
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res=[]
+        def postorder(root):
+            if root==None:
+                return
+            postorder(root.left)
+            postorder(root.right)
+            res.append(root.val)
+        postorder(root)
+        return(res)
+#102
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        Q=[root]
+        l=[]
+        currl=[]
+        Q.append(None)
+        while Q:
+            curr=Q.pop(0)
+            if curr==None:
+                l.append(currl)
+                if len(Q)==0:
+                    return l
+                else: 
+                    currl=[]
+                    Q.append(None)
+            else:
+                currl.append(curr.val)
+                if curr.left:
+                    Q.append(curr.left)
+                if curr.right:
+                    Q.append(curr.right)
+        return l
             
